6bbf2e0e2279d95325af790faa51b30824e7c8ae
mu <mu@mus-MacBook-Pro.local>
2015-10-09 06:26:53 -0700

    Fri Oct  9 06:26:44 PDT 2015

---
+++ test/test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test
+//
+//  Created by mu on 10/9/15.
+//  Copyright © 2015 mu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
